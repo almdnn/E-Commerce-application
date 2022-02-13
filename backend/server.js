59e48b03529cf541ef44ec9bae20a8b7be2f2cc3
Almedin Alibasic <alibasicalmedin@gmail.com>
2022-02-13 04:34:21 +0100

    prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -18,9 +18,7 @@ connectDB();
 const app = express();
 app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.send('Api running...')
-})
+
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
@@ -29,6 +27,16 @@ app.use('/api/upload', uploadRoutes);
 
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+if(process.env.NODE_ENV === 'production'){
+        app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+        app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
+} else {
+    app.get('/', (req, res) => {
+        res.send('Api running...')
+    })
+}
 
 
 app.use(notFound)
